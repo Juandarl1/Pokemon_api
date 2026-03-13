9c9482d63a7f925ca954d65b45a62875ebd6436f
Juan David <juandavidrodriguezlondono@gmail.com>
2026-03-13 18:50:23 -0500

    First Version Api connection

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react"; 
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Pokemon API</h1>
+
+      <section className="Buttons">
+        <button>
+            "
+        </button>
+        <button>
+
+        </button>
+
+      </section>
+
+
+
+
+    </div>
+  );
+}
+
+export {App};
