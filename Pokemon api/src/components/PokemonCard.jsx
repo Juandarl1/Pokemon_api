d2ec1eb6110ea882ab4ffb70a842ec709a5e1dd2
Juan David <juandavidrodriguezlondono@gmail.com>
2026-03-21 16:12:00 -0500

    Added visual for pokemons

--- Pokemon api/src/components/PokemonCard.jsx
+++ Pokemon api/src/components/PokemonCard.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
-export default function PokemonCard() {
+export default function PokemonCard({PokemonData}) {
+    if (!PokemonData) {
+        return <div className="bg-white rounded-lg shadow-md p-4">Loading...</div>;
+    }
     return (
         <div className="bg-white rounded-lg shadow-md p-4">
-            <div>
-            <h2 className="text-xl font-bold mb-2">Pokemon Name</h2>
-            <img src="pokemon-image-url" alt="Pokemon Name" className="w-full h-auto mb-4" />
+            <div className="grid grid-cols-2 gap-4 mb-4">
+            <img src={PokemonData.sprites.front_default} className="w-50px h-auto mb-4" 
+            alt={PokemonData.name}/>
+            <img src={PokemonData.sprites.back_default} className="w-50px h-auto mb-4"
+            alt={PokemonData.name}/>
             </div>
             <h2 className="text-x1 font semibold capitalize">
-                Nombre
+                {PokemonData.name}
             </h2>
-            <p className="text-gray-700">ID: Pokemon ID</p>
-            <p className="text-gray-700">Type: Pokemon Type</p>
-            <p className="text-gray-700">Abilities: Pokemon Abilities</p>
+            <p className="text-gray-700">ID: {PokemonData.id}</p>
+            <p className="text-gray-700">Type: {PokemonData.types.map((type) => type.type.name).join(', ')}</p>
+            <p className="text-gray-700">Abilities: {PokemonData.abilities.map((ability) => ability.ability.name).join(', ')}</p>
         </div>
 
     )
